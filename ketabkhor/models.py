b23b7362e39dadcbafd52fb407b30c5fdc629145
Saeed Masoumi <s-masoumi@live.com>
2016-12-28 13:05:18 +0330

    Add all model without any primary-key or foreign key

--- ketabkhor/models.py
+++ ketabkhor/models.py
@@ -21,3 +21,59 @@ class Order(models.Model):
     address = models.CharField(max_length=511)
     phone_number = models.IntegerField(default=0)
     zip_code = models.IntegerField(default=0)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birthday = models.IntegerField(default=0)
+    user_type = models.IntegerField(default=1)
+    password_hash = models.CharField(max_length=511)
+
+
+class BookReview(models.Model):
+    title = models.CharField(max_length=127)
+    author = models.CharField(max_length=63)
+    context = models.CharField(max_length=511)
+    score = models.IntegerField(default=0)
+
+
+class KetabYar(models.Model):
+    name = models.CharField(max_length=127)
+    address = models.CharField(max_length=511)
+    phone = models.IntegerField(default=0)
+    credit = models.IntegerField(default=0)
+    password_hash = models.CharField(max_length=511)
+
+
+class BlogAdmin(models.Model):
+    name = models.IntegerField(default=0)
+    password_hash = models.CharField(max_length=511)
+
+
+class BlogPost(models.Model):
+    id = models.IntegerField(default=0)
+    title = models.CharField(max_length=127)
+    context = models.CharField(max_length=2047)
+    author = models.CharField(max_length=32)
+    date = models.IntegerField(default=0)
+    time = models.IntegerField(default=0)
+
+
+class BlogPostComment(models.Model):
+    title = models.CharField(max_length=63)
+    context = models.CharField(max_length=511)
+    author = models.CharField(max_length=32)
+    date = models.IntegerField(default=0)
+
+
+class StudyProgram(models.Model):
+    isbn = models.IntegerField(default=0)
+    current_page = models.IntegerField(default=0)
+
+
+class SupportTicket(models.Model):
+    title = models.CharField(max_length=63)
+    context = models.CharField(max_length=511)
+    date = models.IntegerField(default=0)
+    time = models.IntegerField(default=0)
