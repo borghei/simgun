3cf3ba6bef138a3049643541518b4fd5746c02f3
Saeed Masoumi <s-masoumi@live.com>
2016-12-28 12:48:55 +0330

    Add model

--- ketabkhor/models.py
+++ ketabkhor/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Book(models.Model):
+    isbn = models.IntegerField(default=0, primary_key=True)
+    title = models.CharField(max_length=127)
+    author = models.CharField(max_length=63)
+    translator = models.CharField(max_length=63)
+    description = models.CharField(max_length=511)
+    page_count = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=127)
+    price = models.IntegerField(default=0)
+
+
+class Order(models.Model):
+    id = models.IntegerField(default=0, primary_key=True)
+    status = models.CharField(max_length=127)
+    address = models.CharField(max_length=511)
+    phone_number = models.IntegerField(default=0)
+    zip_code = models.IntegerField(default=0)
