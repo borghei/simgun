485b2427ff8cc9995271a02f0a37eca0ca098691
Saeed Masoumi <s-masoumi@live.com>
2016-12-30 16:57:53 +0330

    find related book from db in views#book_details

--- ketabkhor/views.py
+++ ketabkhor/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponse
-
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from ketabkhor.models import Book
 
 
 def book_details(request, isbn):
-    print(isbn)
-    return HttpResponse(" ISBN: ")
+    book = get_object_or_404(Book, pk=isbn)
+    return render(request, "ketabkhor/books/book_details.html",  {'book': book})
 
 
 def home_page(request):
