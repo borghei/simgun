e195e6790cd52689100edacd7f00ed4c33af2d16
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-06 15:34:32 +0330

    change in Book model

--- ketabkhor/models.py
+++ ketabkhor/models.py
@@ -5,14 +5,15 @@ from django.db import models
 
 
 class Book(models.Model):
-    isbn = models.IntegerField(default=0)
+    isbn = models.IntegerField(default=0, null=True)
     title = models.CharField(max_length=127)
     author = models.CharField(max_length=63)
-    translator = models.CharField(max_length=63)
+    translator = models.CharField(max_length=63, default='', null=True)
     description = models.CharField(max_length=511)
     page_count = models.IntegerField(default=0)
     publisher = models.CharField(max_length=127)
     price = models.IntegerField(default=0)
+    pic = models.ImageField(upload_to='static/media/photos/books/', blank=True, null=True, default='')
 
 
 class Address(models.Model):
