83ab14b6fb997464357602c69f217e338e914f71
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-06 19:55:41 +0330

    add birthday field to user profile model

--- ketabkhor/models.py
+++ ketabkhor/models.py
@@ -22,6 +22,7 @@ class Book(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    birthday = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='static/media/photos/profiles/', blank=True, null=True,
                                default='static/media/photos/profiles/matthew.png')
 
