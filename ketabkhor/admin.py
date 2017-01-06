ee085ad76aed076b2dade8c2580dabbcc6a3f2eb
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-06 19:58:29 +0330

    Registration is now already created

--- ketabkhor/admin.py
+++ ketabkhor/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from ketabkhor.models import Book
+from ketabkhor.models import Book, UserProfile
 
 admin.site.register(Book)
+admin.site.register(UserProfile)
