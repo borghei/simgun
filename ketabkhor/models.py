690bf20871ebe359089bf19207a103dbaee3232b
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-06 13:53:47 +0330

    Refactoring in models.py

--- ketabkhor/models.py
+++ ketabkhor/models.py
@@ -5,63 +5,35 @@ from django.db import models
 
 
 class Book(models.Model):
-    isbn = models.IntegerField(default=0, primary_key=True)
-    title = models.CharField(max_length=127)
-    author = models.CharField(max_length=63)
-    translator = models.CharField(max_length=63)
-    description = models.CharField(max_length=511)
+    isbn = models.IntegerField(default=0)
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=64)
+    translator = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
     page_count = models.IntegerField(default=0)
-    publisher = models.CharField(max_length=127)
+    publisher = models.CharField(max_length=128)
     price = models.IntegerField(default=0)
 
 
 class Address(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     address = models.CharField(max_length=511)
     phone_number = models.IntegerField(default=0)
     zip_code = models.IntegerField(default=0)
 
 
 class Order(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     status = models.CharField(max_length=127)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, default='')
 
 
-class User(models.Model):
-    user_name = models.CharField(max_length=32, primary_key=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    birthday = models.IntegerField(default=0)
-    user_type = models.IntegerField(default=1)
-    password_hash = models.CharField(max_length=511)
-
-
 class BookReview(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=127)
     author = models.CharField(max_length=63)
     context = models.CharField(max_length=511)
     rate = models.IntegerField(default=0)
 
 
-class Vendor(models.Model):
-    user_name = models.CharField(max_length=32, primary_key=True)
-    name = models.CharField(max_length=127)
-    address = models.CharField(max_length=511)
-    phone_number = models.IntegerField(default=0)
-    rate = models.IntegerField(default=0)
-    password_hash = models.CharField(max_length=511)
-
-
-class BlogAdmin(models.Model):
-    user_name = models.CharField(max_length=32, primary_key=True)
-    name = models.IntegerField(default=0)
-    password_hash = models.CharField(max_length=511)
-
-
 class BlogPost(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=127)
     context = models.CharField(max_length=2047)
     author = models.CharField(max_length=32)
@@ -69,7 +41,6 @@ class BlogPost(models.Model):
 
 
 class BlogComment(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=63)
     context = models.CharField(max_length=511)
     author = models.CharField(max_length=32)
@@ -77,12 +48,10 @@ class BlogComment(models.Model):
 
 
 class ReadingProgram(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     current_page = models.IntegerField(default=0)
 
 
 class SupportTicket(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=63)
     context = models.CharField(max_length=511)
     date = models.DateTimeField()
