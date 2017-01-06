984369852a689f68a8dddd5196ac911180bbc4c0
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-06 19:56:50 +0330

    Register view created

--- ketabkhor/views.py
+++ ketabkhor/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import generic
 
-from ketabkhor.models import Book
-
+from ketabkhor.forms import UserRegisterForm
+from ketabkhor.models import Book, UserProfile
+from django.contrib.auth.models import User
 
 def home_page(request):
     return render(request, "ketabkhor/index.html")
@@ -12,3 +13,32 @@ def home_page(request):
 class BookDetails(generic.DetailView):
     model = Book
     template_name = "ketabkhor/book-detail.html"
+
+
+def user_register(request):
+    if request.method == 'GET':
+        return render(request, 'ketabkhor/register.html', {'register': False, 'register_form': UserRegisterForm()})
+
+    elif request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password'),
+                first_name=form.cleaned_data.get('firstname'),
+                last_name=form.cleaned_data.get('lastname'),
+                email=form.cleaned_data.get('email'),
+            )
+
+            user.save()
+
+            user_profile = UserProfile(
+                user=user,
+                birthday=form.cleaned_data.get('birthday')
+            )
+
+            user_profile.save()
+
+            return render(request, 'ketabkhor/register.html', {'register': True, 'register_form': form})
+        else:
+            return render(request, 'ketabkhor/register.html', {'register': False, 'register_form': form})
