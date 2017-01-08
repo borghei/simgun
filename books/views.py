9a300a16ca19949db2daa2e10c9c28c97a335d81
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-09 00:43:04 +0330

    book review created

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.urls import reverse
 from django.views import generic
 
+from accounts.models import UserProfile
 from books.models import Book, BookReview
 
 
@@ -13,8 +16,16 @@ class BookDetails(generic.DetailView):
 
 
 def add_book_review(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    user_profile = get_object_or_404(Book, user=request.user)
-    book_review = BookReview(user_profile=user_profile, book=book)
-    book_review.save()
-    return JsonResponse({'status': 'ok'})
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=book_id)
+        user_profile = get_object_or_404(UserProfile, user=request.user)
+        print('text: ' + str(request.POST.get('text')))
+        print('title: ' + str(request.POST.get('title')))
+        book_review = BookReview(user_profile=user_profile,
+                                 book=book,
+                                 title=request.POST.get('title'),
+                                 text=request.POST.get('text'))
+        book_review.save()
+        return JsonResponse({'status': 'ok', 'url': reverse('books:book_details', args=(book_id,))})
+    else:
+        return JsonResponse({'status': 'failure'})
