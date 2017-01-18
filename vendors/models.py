0df0fd6dbcfd7b0182defa1ff5bdd96a6a37cff7
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-01-18 16:51:33 +0330

    remove Vendor model and create 'vendors' app

--- vendors/models.py
+++ vendors/models.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# Create your models here.
 
-class UserProfile(models.Model):
+
+class Vendor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthday = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='static/site-media/photos/profiles/', blank=True, null=True,
                                default='static/site-media/photos/profiles/matthew.png')
-
-    def __str__(self):
-        return str(self.user)
