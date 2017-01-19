ced28d5586849819fea8d9791d8e88a7a9c9e53f
Saeed Masoumi <s-masoumi@live.com>
2017-01-19 13:38:41 +0330

    Add advanced search

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,9 +8,27 @@ from books.models import Book
 
 
 def search(request):
-    title = request.GET['q']
-    books = Book.objects.filter(title__contains=title)
     return render(request, 'search/advanced-search.html', {
-        'title': title,
-        'books': books
+        'title': "جستجو",
+        'books': advanced_search(request)
     })
+
+
+def advanced_search(request):
+    # It's easier to store a dict of the possible lookups we want, where
+    # the values are the keyword arguments for the actual query.
+    qdict = {'q': 'title__contains',
+             'isbn': 'isbn__contains',
+             'author': 'author__contains',
+             'translator': 'translator__contains',
+             'publisher': 'publisher__contains',
+             }
+
+    # Then we can do this all in one step instead of needing to call
+    # 'filter' and deal with intermediate data structures.
+    q_objs = [Q(**{qdict[k]: request.GET[k]}) for k in qdict.keys() if k in request.GET]
+    print(q_objs)
+    search_results = Book.objects.select_related().filter(*q_objs)
+    if 'min_price' in request.GET and 'max_price' in request.GET:
+        search_results = search_results.filter(price__range=(request.GET['min_price'], request.GET['max_price']))
+    return search_results
