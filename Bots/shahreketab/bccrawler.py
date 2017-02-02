a532259e723c3bb7fb35d7ceaaf53ac41db6c441
Shervin <sv.manzouri@gmail.com>
2017-02-02 23:58:41 +0330

    create basic scraper

--- Bots/shahreketab/bccrawler.py
+++ Bots/shahreketab/bccrawler.py
@@ -2,31 +2,10 @@ __author__ = 'Shervin'
 from bs4 import BeautifulSoup
 import requests
 
-# url = "http://shahreketabonline.com"
-# r = requests.get(url)
-#
-# soup = BeautifulSoup(r.content, "html.parser") #html content related to that specific request
-# for link in soup.find_all("a"):
-#     print(link.text, " ", link.get("href"))
+url = "http://shahreketabonline.com"
+r = requests.get(url)
 
-url = "http://shahreketabonline.com/products/43/163698/%D8%B3%D9%81%D8%B1%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%85%DB%8C%D9%88%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%88_%D8%AA%D8%B2_%DB%8C%D9%86%D8%A7%D8%AA_%D9%BE%D8%A7%D9%86%DB%8C%D8%B0"
-req = requests.get(url)
-soup = BeautifulSoup(req.content, "html.parser")
-
-book_isbn = soup.find("span", {"itemprop": "isbn"})
-book_title = soup.find("span", {"itemprop": "name"})
-book_author = soup.find("span", {"itemprop": "author"})
-book_pagecount = soup.find("span", {"itemprop": "numberOfPages"})
-book_publisher = soup.find("span", {"itemprop": "publisher"})
-book_price = soup.find("span", {"itemprop": "price"})
-
-#book_pic = soup.find("span", {"itemprop": "isbn"})
-book_translator = soup.find("span", {"itemprop": "isbn"})
-book_description = soup.find_all("div", {"itemprop": "description"})
-book_category = soup.find("span", {"style": "text-align: justify;"})
-
-
-
-
-print(g_data)
+soup = BeautifulSoup(r.content, "html.parser") #html content related to that specific request
+for link in soup.find_all("a"):
+    print(link.text, " ", link.get("href"))
 
