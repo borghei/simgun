b003aeb72093686f2e612c5829f7491da24c82f9
Shervin <sv.manzouri@gmail.com>
2017-02-03 03:19:30 +0330

    refactor scraping further, merge functions

--- Bots/shahreketab/bccrawler.py
+++ Bots/shahreketab/bccrawler.py
@@ -10,28 +10,18 @@ def scrape_page(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
 
-    book_isbn = soup.find("span", {"itemprop": "isbn"}).text
-    book_title = soup.find("span", {"itemprop": "name"}).text
-    book_author = soup.find("span", {"itemprop": "author"}).text
-    book_pagecount = soup.find("span", {"itemprop": "numberOfPages"}).text
-    book_publisher = soup.find("span", {"itemprop": "publisher"}).text
-    book_price = soup.find("span", {"itemprop": "price"}).text
-    book_description = [x.text for x in soup.find_all("div", {"style": "text-align: justify;"})][2::]
-    book_category = soup.find("label", text = "دسته‌بندی").next_sibling[3:]
-
-    info['isbn'] = book_isbn
-    info['title'] = book_title
-    info['author'] = book_author
-    info['description'] = book_description
-    info['page_count'] = book_pagecount
-    info['publisher'] = book_publisher
-    info['price'] = book_price
-    info['category'] = book_category
+    info['isbn'] = soup.find("span", {"itemprop": "isbn"}).text
+    info['title'] = soup.find("span", {"itemprop": "name"}).text
+    info['author'] = soup.find("span", {"itemprop": "author"}).text
+    info['description'] = [x.text for x in soup.find_all("div", {"style": "text-align: justify;"})][2::]
+    info['page_count'] = soup.find("span", {"itemprop": "numberOfPages"}).text
+    info['publisher'] = soup.find("span", {"itemprop": "publisher"}).text
+    info['price']  = soup.find("span", {"itemprop": "price"}).text
+    info['category'] = soup.find("label", text = "دسته‌بندی").next_sibling[3:]
 
     image_source = soup.find("img", {"class": "full-image img-responsive"})['src']
-    book_pic_path = os.path.join(source_path,str(book_isbn)+".jpg")
-    info['pic'] = book_pic_path
-    with open(book_pic_path, "wb") as file:
+    info['pic'] = os.path.join(source_path,str(info['isbn'])+".jpg")
+    with open(info['pic'], "wb") as file:
         response = requests.get(image_source)
         file.write(response.content)
 
@@ -39,5 +29,5 @@ def scrape_page(url):
         book_translator = soup.find("label", text = "\n            مترجم            :").parent.find("strong").text
         info['translator'] = book_translator
     except:
-        book_translator = None
+        info['translator'] = ""
     return info
