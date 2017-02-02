2ab1aa72e94ec66890829f662ce4fd9358fa0df2
Shervin <sv.manzouri@gmail.com>
2017-02-02 23:59:16 +0330

    scrape shahreketab for page info

--- Bots/shahreketab/bccrawler.py
+++ Bots/shahreketab/bccrawler.py
@@ -2,10 +2,15 @@ __author__ = 'Shervin'
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://shahreketabonline.com"
-r = requests.get(url)
+# url = "http://shahreketabonline.com"
+# r = requests.get(url)
+#
+# soup = BeautifulSoup(r.content, "html.parser") #html content related to that specific request
+# for link in soup.find_all("a"):
+#     print(link.text, " ", link.get("href"))
 
-soup = BeautifulSoup(r.content, "html.parser") #html content related to that specific request
-for link in soup.find_all("a"):
-    print(link.text, " ", link.get("href"))
+url = "http://shahreketabonline.com/products/43/163698/%D8%B3%D9%81%D8%B1%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%85%DB%8C%D9%88%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%88_%D8%AA%D8%B2_%DB%8C%D9%86%D8%A7%D8%AA_%D9%BE%D8%A7%D9%86%DB%8C%D8%B0"
+req = requests.get(url)
+soup = BeautifulSoup(req.content, "html.parser")
+
 
