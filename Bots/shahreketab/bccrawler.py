0a11c9efa26544f78aad50746890a19f1ca634ee
Shervin <sv.manzouri@gmail.com>
2017-02-03 01:27:53 +0330

    refactor image saving and clean up code

--- Bots/shahreketab/bccrawler.py
+++ Bots/shahreketab/bccrawler.py
@@ -5,12 +5,6 @@ import urllib
 import os
 
 source_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
-# url = "http://shahreketabonline.com"
-# r = requests.get(url)
-#
-# soup = BeautifulSoup(r.content, "html.parser") #html content related to that specific request
-# for link in soup.find_all("a"):
-#     print(link.text, " ", link.get("href"))
 
 url = "http://shahreketabonline.com/products/43/163698/%D8%B3%D9%81%D8%B1%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%85%DB%8C%D9%88%D9%87_%D8%A2%D8%B1%D8%A7%DB%8C%DB%8C_%D9%88_%D8%AA%D8%B2_%DB%8C%D9%86%D8%A7%D8%AA_%D9%BE%D8%A7%D9%86%DB%8C%D8%B0"
 req = requests.get(url)
@@ -24,16 +18,16 @@ book_publisher = soup.find("span", {"itemprop": "publisher"}).text
 book_price = soup.find("span", {"itemprop": "price"}).text
 book_description = [x.text for x in soup.find_all("div", {"style": "text-align: justify;"})][2::]
 
-book_pic_url = soup.find("img", {"class": "full-image img-responsive"})['src']
-fullimagename = os.path.join(source_path,str(book_isbn)+".jpg")
-with open(fullimagename, "wb") as file:
-    response = requests.get(book_pic_url)
+image_source = soup.find("img", {"class": "full-image img-responsive"})['src']
+book_pic_path = os.path.join(source_path,str(book_isbn)+".jpg")
+with open(book_pic_path, "wb") as file:
+    response = requests.get(image_source)
     file.write(response.content)
 
 #book_translator = soup.find("span", {"itemprop": "isbn"})
 #book_category = soup.find("span", {"itemprop": "isbn"})
 
-print(book_pic_url)
+print(image_source)
 print(book_isbn)
 print(book_price)
 print(book_pagecount)
