ca37a93254e18f69a07dde9eb0cb6a7d59696c16
rajabzz <saeed.rajabzadeh.95@gmail.com>
2017-02-03 18:35:28 +0330

    Delete redundant method

--- static/general/js/generic-script.js
+++ static/general/js/generic-script.js
@@ -75,12 +75,3 @@ $(document).ready(function() {
     });
 
 });
-
-
-$('#search-bar').keypress(function (e) {
-    if (e.which == 13) {
-        var queryLength = $(this).val().length;
-        if (queryLength > 1 && queryLength < 100)
-            window.location.href = '/search/?best=' + $('#search-bar').val();
-    }
-});
