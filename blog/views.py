e230c7360ae593f3168f80d03b20828ede8837bc
Saeed Masoumi <s-masoumi@live.com>
2017-02-03 19:56:38 +0330

    implement ui for blog home page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 # Create your views here.
+from blog.models import Post
+
+
 def blog_home_page(request):
-    return HttpResponse("home_page")
+    posts = get_list_or_404(Post.objects.all())
+    return render(request, 'blog/blog-home.html', {
+        'posts': posts
+    })
 
 
 def blog_post(request, post_id):
-    return HttpResponse("post")
+    post = get_object_or_404(Post, pk=post_id)
+    return HttpResponse("post %s" % post)
