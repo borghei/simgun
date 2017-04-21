e9740756c70b6fa4fac94515f4f457419cf3f999
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2017-04-21 22:07:42 +0430

    removed blog app

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from blog import views
-
-app_name = 'blog'
-urlpatterns = [
-    url(r'^$', views.blog_home_page, name='blog_home_page'),
-    url(r'^(?P<post_id>[0-9]+)/$', views.blog_post, name='blog_post'),
-]
