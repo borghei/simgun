e09042cd85ebfa3b6b560d03b182d64ecf3e4721
Ehsan <ehsunouri@gmail.com>
2017-05-13 22:28:58 +0430

    Add required fields to product model

--- product/models.py
+++ product/models.py
@@ -12,33 +12,41 @@ class Category(models.Model):
         return self.title
 
 
+def _get_image_filename():
+    return 'static/site-media/photos/product/'
+
+
 class Product(models.Model):
     title = models.CharField(max_length=127)
     description = models.CharField(max_length=2048)  # max_length is in chars
     price = models.IntegerField(default=0)
-    mainPic = models.ImageField(upload_to='static/site-media/photos/product/', blank=True, null=True, default='')
+    mainPic = models.ImageField(upload_to=_get_image_filename, blank=True, null=True, default='')
     # todo add array of pics
-    # add count field
-    # weigth
-    # add order's count
-
-    #
+    itemCount = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    orderCount = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.title + ' - ' + self.publisher
+        return self.title  # + ' - ' + self.publisher
 
-    def get_avg_rating(self):
-        book_rate_avg = 0
-        all_bookratings = self.bookrating_set.all()
-        all_bookratings_count = all_bookratings.count()
-        if all_bookratings_count == 0:
-            return 0
-        for book_rate in all_bookratings:
-            book_rate_avg += book_rate.rate
-        book_rate_avg /= all_bookratings.count()
-        book_rate_avg = float('{0: .2f}'.format(book_rate_avg))
-        return book_rate_avg
+    # def get_avg_rating(self):
+    #     book_rate_avg = 0
+    #     all_bookratings = self.bookrating_set.all()
+    #     all_bookratings_count = all_bookratings.count()
+    #     if all_bookratings_count == 0:
+    #         return 0
+    #     for book_rate in all_bookratings:
+    #         book_rate_avg += book_rate.rate
+    #     book_rate_avg /= all_bookratings.count()
+    #     book_rate_avg = float('{0: .2f}'.format(book_rate_avg))
+    #     return book_rate_avg
+
+
+class ProductImage(models.Model):
+    post = models.ForeignKey(Product, default=None)
+    image = models.ImageField(upload_to=_get_image_filename,
+                              verbose_name='Image', )
 
 
 class Review(models.Model):
